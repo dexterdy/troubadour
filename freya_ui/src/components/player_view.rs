4e26c0590b4a45ba63c234c4d793dd06577aa9d3
dexterdy <phagedoorn@bkwi.nl>
2025-07-23 08:46:29 +0200

    error handling for player view

--- freya_ui/src/components/player_view.rs
+++ freya_ui/src/components/player_view.rs
@@ -3,11 +3,14 @@ use crate::{player_ref::PlayerRef, AppState};
 use duration_human::DurationHuman;
 use freya::prelude::*;
 use std::time::Duration;
+use anyhow::Error;
+use crate::common_actions::ShowError;
 
 #[component]
 pub fn PlayerView(player: PlayerRef, state: Signal<AppState>) -> Element {
     let player_clone = player.clone();
     let player_borrow = player_clone.read();
+    let mut show_error_popup = use_context::<UsePopup<Error, ShowError>>();
 
     let player_clone = player.clone();
     let is_playing = use_polling(
@@ -24,21 +27,25 @@ pub fn PlayerView(player: PlayerRef, state: Signal<AppState>) -> Element {
     let player_clone = player.clone();
     let play = move |_| {
         player_clone.with_mut(|p| {
-            let _ = p.play();
+            if let Err(e) = p.play() {
+                spawn(async move {
+                    show_error_popup.open(Some(e.into())).await;
+                });
+            }
         });
     };
 
     let player_clone = player.clone();
     let pause = move |_| {
         player_clone.with_mut(|p| {
-            let _ = p.pause();
+            p.pause();
         });
     };
 
     let player_clone = player.clone();
     let stop = move |_| {
         player_clone.with_mut(|p| {
-            let _ = p.stop();
+            p.stop();
         });
     };
 
@@ -50,61 +57,81 @@ pub fn PlayerView(player: PlayerRef, state: Signal<AppState>) -> Element {
         state.write().saved = false;
     };
 
-    // let mut cut_start_input = use_signal(|| duration_to_string(player_borrow.cut_start, false));
-    // let player_clone = player.clone();
-    // let cut_start = move |new_cut: String| {
-    //     cut_start_input.set(new_cut.clone());
-    //     if let Ok(cut) = duration_str::parse(new_cut) {
-    //         player_clone.with_mut(|p| {
-    //             let _ = p.cut_start(cut);
-    //         });
-    //     }
-    //     state.write().saved = false;
-    // };
-
-    // let mut cut_end_input = use_signal(|| duration_to_string(player_borrow.cut_end, false));
-    // let player_clone = player.clone();
-    // let cut_end = move |new_cut: String| {
-    //     cut_end_input.set(new_cut.clone());
-    //     if let Ok(cut) = duration_str::parse(new_cut) {
-    //         player_clone.with_mut(|p| {
-    //             let _ = p.cut_end(cut);
-    //         });
-    //     }
-    //     state.write().saved = false;
-    // };
-
     let player_clone = player.clone();
     let toggle_loop = move |_| {
         player_clone.with_mut(|p| {
-            let _ = p.toggle_loop(!p.looping, p.loop_gap);
+            if let Err(e) = p.toggle_loop(!p.looping, p.loop_gap) {
+                spawn(async move {
+                    show_error_popup.open(Some(e.into())).await;
+                });
+            }
         });
         state.write().saved = false;
     };
 
-    // let mut loop_gap_input = use_signal(|| duration_to_string(player_borrow.loop_gap, false));
-    // let player_clone = player.clone();
-    // let set_loop_gap = move |new_loop_gap: String| {
-    //     loop_gap_input.set(new_loop_gap.clone());
-    //     if let Ok(gap) = duration_str::parse(new_loop_gap) {
-    //         player_clone.with_mut(|p| {
-    //             let _ = p.toggle_loop(p.looping, gap);
-    //         });
-    //     }
-    //     state.write().saved = false;
-    // };
+    let mut cut_start_input = use_signal(|| duration_to_string(player_borrow.cut_start, false));
+    let player_clone = player.clone();
+    let cut_start = move |new_cut: String| {
+        cut_start_input.set(new_cut.clone());
+        if let Ok(cut) = duration_str::parse(new_cut) {
+            player_clone.with_mut(|p| {
+                if let Err(e) = p.cut_start(cut) {
+                    spawn(async move {
+                        show_error_popup.open(Some(e.into())).await;
+                    });
+                }
+            });
+        }
+        state.write().saved = false;
+    };
 
-    // let mut delay_input = use_signal(|| duration_to_string(player_borrow.delay_length, false));
-    // let player_clone = player.clone();
-    // let set_delay = move |new_delay: String| {
-    //     delay_input.set(new_delay.clone());
-    //     if let Ok(delay) = duration_str::parse(new_delay) {
-    //         player_clone.with_mut(|p| {
-    //             let _ = p.set_delay(delay);
-    //         });
-    //     }
-    //     state.write().saved = false;
-    // };
+    let mut cut_end_input = use_signal(|| duration_to_string(player_borrow.cut_end, false));
+    let player_clone = player.clone();
+    let cut_end = move |new_cut: String| {
+        cut_end_input.set(new_cut.clone());
+        if let Ok(cut) = duration_str::parse(new_cut) {
+            player_clone.with_mut(|p| {
+                if let Err(e) = p.cut_end(cut) {
+                    spawn(async move {
+                        show_error_popup.open(Some(e.into())).await;
+                    });
+                }
+            });
+        }
+        state.write().saved = false;
+    };
+
+    let mut loop_gap_input = use_signal(|| duration_to_string(player_borrow.loop_gap, false));
+    let player_clone = player.clone();
+    let set_loop_gap = move |new_loop_gap: String| {
+        loop_gap_input.set(new_loop_gap.clone());
+        if let Ok(gap) = duration_str::parse(new_loop_gap) {
+            player_clone.with_mut(|p| {
+                if let Err(e) = p.toggle_loop(p.looping, gap) {
+                    spawn(async move {
+                        show_error_popup.open(Some(e.into())).await;
+                    });
+                }
+            });
+        }
+        state.write().saved = false;
+    };
+
+    let mut delay_input = use_signal(|| duration_to_string(player_borrow.delay_length, false));
+    let player_clone = player.clone();
+    let set_delay = move |new_delay: String| {
+        delay_input.set(new_delay.clone());
+        if let Ok(delay) = duration_str::parse(new_delay) {
+            player_clone.with_mut(|p| {
+                if let Err(e) = p.set_delay(delay) {
+                    spawn(async move {
+                        show_error_popup.open(Some(e.into())).await;
+                    });
+                }
+            });
+        }
+        state.write().saved = false;
+    };
 
     rsx! {
         rect {
@@ -164,14 +191,6 @@ pub fn PlayerView(player: PlayerRef, state: Signal<AppState>) -> Element {
             }
         }
     }
-    // label { "cut start" }
-    // Input { value: cut_start_input, onchange: cut_start }
-    // label { "cut end" }
-    // Input { value: cut_end_input, onchange: cut_end }
-    // label { "loop gap" }
-    // Input { value: loop_gap_input, onchange: set_loop_gap }
-    // label { "delay" }
-    // Input { value: delay_input, onchange: set_delay }
 }
 
 pub fn duration_to_string(dur: Duration, no_smaller_than_secs: bool) -> String {
